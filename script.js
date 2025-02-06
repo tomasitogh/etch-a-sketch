c4ae2492b698506f622d3caa618764fb23b44621
Tomas Gonzalez Humphreys <tomasgonzalezh@MacBookAir.fibertel.com.ar>
2025-02-06 00:40:16 -0300

    Hover feature added, grids display just fine only with flexbox. It woks succesfully.

--- script.js
+++ script.js
@@ -4,18 +4,33 @@ const btn = document.querySelector("#input-button");
 
 //event listener tu submit the number in the input value
 btn.addEventListener("click", () => {
-    const currentValue = inpt.value;
-    inpt.value = '';
-    createGrid(currentValue);
+    if (inpt.value > 100){
+        alert("Try again, no more than 100");
+        return;
+    }
+    createGrid();
 });
 
 
 //Function that receives de amount of grids inserted by  user 
 // and creates and inserts the div grids
-function createGrid(number){
-    for (i = 1; i <= number * number; i++){
+function createGrid(){
+    const numberGrids = parseInt(inpt.value) || 16;
+    inpt.value = '';
+
+    container.textContent = '';
+
+    let totalSize = 500;
+    let gridSize = totalSize / numberGrids; 
+
+    for (i = 0; i < numberGrids * numberGrids; i++){
         const grids = document.createElement("div");
         grids.classList.add("grid-squares");
+        grids.style.cssText = `width: ${gridSize}px; height: ${gridSize}px;`;
+        grids.style.flex = `0 0 ${gridSize}px`; 
+        grids.addEventListener("mouseover", () => {
+            grids.style.backgroundColor = "black";
+        })
         container.appendChild(grids);
     };
 };
